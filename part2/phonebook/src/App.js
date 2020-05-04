4a570822e4b4787aec507b5b7fc0f56f8e75aa22
Suzanne Maquelin <slmaquelin@gmail.com>
2020-05-04 12:56:27 +0200

    phonebook 2.19-2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,17 @@ import personService from './services/persons'
 import Persons from './components/persons'
 import Filter from './components/filter'
 import PersonForm from './components/personform'
+import Notification from './components/notification'
+import ErrorNotification from './components/errornotification'
+import './index.css'
 
 const App = () => {
   const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchName, setSearchName ] = useState('')
+  const [ notification, setNotification ] = useState(null)
+  const [ errorNotification, setErrorNotification] = useState(null)
 
   useEffect(() => {
     personService
@@ -50,8 +55,17 @@ const App = () => {
       const id = persons.find(person => person.name == newName).id
       personService.update(id, personObject)
         .then(returnedPerson => {
-          console.log(returnedPerson)
           setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+          setNotification(`Number of ${returnedPerson.name} is changed`)
+          setTimeout(() => {
+            setNotification(null)
+          }, 3000)
+        })
+        .catch(error => {
+          setErrorNotification(`Information of ${personObject.name} has already been removed from server`)
+          setTimeout(() => {
+            setErrorNotification(null)
+          }, 3000)
         })
     }
   }
@@ -61,8 +75,17 @@ const App = () => {
       .create(personObject)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
+          setNotification(`Added ${returnedPerson.name}`)
+          setTimeout(() => {
+            setNotification(null)
+          }, 3000)
         })
-      .catch(error => {console.log('fail')})
+      .catch(error => {
+        setErrorNotification(`Adding ${personObject.name} failed`)
+        setTimeout(() => {
+          setErrorNotification(null)
+        }, 3000)
+      })
   }
 
   const handleDeletePerson = (person) => {
@@ -72,12 +95,21 @@ const App = () => {
         .then(() => {
           setPersons(persons.filter(p => p.name != person.name))
         })
+        .catch(error => {
+          setErrorNotification(`Deleting ${person.name} failed`)
+          setTimeout(() => {
+            setErrorNotification(null)
+          }, 3000)
+        })
       : console.log('not deleted due to cancelation')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <ErrorNotification message={errorNotification}/>
+
+      <Notification message={notification}/>
       
       <Filter value={searchName} handler={handleSearch}/>
 
